670a42250cc70544e2bd81047a0928c6fa138d46
MahtabSinghAujla <125728731+MahtabSinghAujla@users.noreply.github.com>
2026-09-02 19:32:59 +1000

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,33 +1,60 @@
-const int In1 = 6;
-const int In2 = 5;
-const int EnA = 7; 
+const int EnA=5;
+const int In1=7;
+const int In2=6;
+const int In3=4;
+const int In4=2;
+const int EnB=3;
 
-const int In3 = 4;
-const int In4 = 3;
-const int EnB = 2; 
+const int pingPin=8;
 
-void setup(){
-  // All motor control pins are outputs
+void setup() {
+  Serial.begin(9600);
+
   pinMode(In1, OUTPUT);
   pinMode(In2, OUTPUT);
-
-  pinMode(EnA, OUTPUT); 
+  pinMode(EnA, OUTPUT);
   pinMode(In3, OUTPUT);
   pinMode(In4, OUTPUT);
-  pinMode(EnB, OUTPUT); 
-  Serial.begin(9600);
+  pinMode(EnB, OUTPUT);
 }
 
-void goStraight() {  //run both motors in the same direction
-  // turn on motor A
-  digitalWrite(In1, HIGH);
-  digitalWrite(In2, LOW);
-  analogWrite(EnA, 150);
+void loop() {
+  digitalWrite(In1, LOW);
+  digitalWrite(In2, HIGH);
+  analogWrite(EnA, 0);
+
   digitalWrite(In3, HIGH);
   digitalWrite(In4, LOW);
-  analogWrite(EnB, 150);
+  analogWrite(EnB, 0);
+
+  // establish variables for duration of the ping, 
+  // and the distance result centimetres:
+  long duration, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  // Change the pin mode, as we will now read in on this pin  
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+
+  // convert the time into a distance
+  cm = microsecondsToCentimetres(duration);
+  
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();  
+  delay(100);
 }
 
-void loop(){
-  goStraight();
+long microsecondsToCentimetres(long microseconds)
+{
+  return microseconds / 29 / 2;
 }
